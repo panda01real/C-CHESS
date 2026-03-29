c71cfdcc642d426b811104a1c10a5b52bed5da2b
panda01real <marcospereyra07@gmail.com>
2026-03-29 02:52:48 -0300

    avance con la clase del tablero
    hecho el constructor que inicializa el tablero con sus posiciones.
    creado los metodos escenciales de la clase
    setpiece, getpiece, y drawboard

--- C CHESS/Chessboard.cs
+++ C CHESS/Chessboard.cs
@@ -2,5 +2,28 @@
 
 public class Chessboard
 {
-    
+    private Piece[,] _squares; // array de piezas para poder acceder a cada pieza más facil
+
+    public Chessboard()
+    {
+        _squares = new Piece[8, 8];
+    }
+
+    public void SetPiece(int x, int y, Piece piece)
+    {
+        //metodo para spawnear una pieza en la posicion indicada
+        //chequear si ya hay una pieza en la posicion indicada
+        //_squares[x, y] = new Int32;
+    }
+
+    public void GetPiece(int x, int y)
+    {
+        // imprimir que pieza esta en la posicion indicada
+    }
+
+    public void DrawBoard()
+    {
+        //Dibuja una preview del gamestate
+    }
+
 }
