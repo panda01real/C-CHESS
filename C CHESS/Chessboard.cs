b8970e3a5f445a5881788f6c5eff071f220e23dc
panda01real <marcospereyra07@gmail.com>
2026-03-29 20:40:34 -0300

    metodo setpiece terminada

--- C CHESS/Chessboard.cs
+++ C CHESS/Chessboard.cs
@@ -2,21 +2,35 @@
 
 public class Chessboard
 {
-    private Piece[,] _squares; // array de piezas para poder acceder a cada pieza más facil
+    private Piece?[,] _squares; // array de piezas para poder acceder a cada pieza más facil
+    // SI AGREGO "?" LUEGO DE EL TIPO EN UN ARRAY ESTOY INDICANDO QUE QUIERO QUE HAYAN VALORES NULL.
 
     public Chessboard()
     {
         _squares = new Piece[8, 8];
     }
 
-    public void SetPiece(int x, int y, Piece piece)
+    public void SetPiece(int row, int column, Piece piece)
     {
-        //metodo para spawnear una pieza en la posicion indicada
-        //chequear si ya hay una pieza en la posicion indicada
-        //_squares[x, y] = new Int32;
+        //Chequear si el input es valido
+        if ((row >= 8|| column >= 8) || (row < 0 || column < 0))
+        {
+            Console.WriteLine("Invalid row or column");
+            return;
+        }
+        //Chequear si la posicion esta ocupada, para ocuparla
+        if (_squares[row, column] == null)
+        {
+            //Colocar pieza
+            _squares[row, column] = piece;
+        }
+        else
+        {
+            Console.WriteLine("There's already a piece in this square!");
+        }
     }
 
-    public void GetPiece(int x, int y)
+    public void GetPiece(int row, int column)
     {
         // imprimir que pieza esta en la posicion indicada
     }
--- C CHESS/Program.cs
+++ C CHESS/Program.cs
@@ -7,7 +7,8 @@ namespace ChessGUI
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-			
+			Chessboard _board = new Chessboard();
+			//Console.WriteLine(_board[2,2]);
 		}
 	}
 }
